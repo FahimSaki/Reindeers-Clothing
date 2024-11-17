84d9c8d241f18e3cdcbdb3c7ea26f4f0ac31bf1f
FahimSaki <fahimuzzamaninferno@gmail.com>
2024-11-17 15:08:24 +0600

    Initial Setup Done

---
+++ lib/splash_screen.dart
@@ -0,0 +1,47 @@
+import 'package:ecommerce_ui/pages/intro_page.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(
+      const Duration(seconds: 2),
+      () {
+        if (context.mounted) {
+          // Navigate to the intro page after a delay
+          Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => const IntroPage()),
+          );
+        }
+      },
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.cyan[300],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/logo.png', width: 170, height: 170),
+            const SizedBox(height: 10),
+            const Align(
+              alignment: Alignment.centerRight,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30.0),
+                child: Text(
+                  '℗ by Fahim Saki',
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
