902e7c5b7df7ca44def14936a779a4850ff3a487
FahimSaki <fahimuzzamaninferno@gmail.com>
2024-11-17 22:07:29 +0600

    Theme Applied

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_ui/splash_screen.dart';
+import 'package:ecommerce_ui/themes/light_mode.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,9 +11,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      home: const SplashScreen(),
+      theme: lightMode,
     );
   }
 }
