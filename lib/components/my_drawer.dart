660a3b5039e0783d6f65c3aa774d03e7cf94b600
FahimSaki <fahimuzzamaninferno@gmail.com>
2024-11-27 00:32:14 +0600

    Drawer Fixed

--- lib/components/my_drawer.dart
+++ lib/components/my_drawer.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_ui/components/my_list_tile.dart';
 import 'package:flutter/material.dart';
 
 class MyDrawer extends StatelessWidget {
@@ -8,19 +9,59 @@ class MyDrawer extends StatelessWidget {
     return Drawer(
       backgroundColor: Theme.of(context).colorScheme.surface,
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          // drawer logo
-          DrawerHeader(
-              child: Center(
-            child:
-                Image.asset('assets/images/logo.png', width: 170, height: 170),
-          ))
+          Column(
+            children: [
+              // drawer logo
+              Theme(
+                data: Theme.of(context).copyWith(
+                  dividerTheme:
+                      const DividerThemeData(color: Colors.transparent),
+                ),
+                child: DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context)
+                        .colorScheme
+                        .surface, // Use your drawer background color
+                  ),
+                  child: Center(
+                    child: Image.asset(
+                      'assets/images/logo.png',
+                      width: 170,
+                      height: 170,
+                    ),
+                  ),
+                ),
+              ),
 
-          // shop tile
+              const SizedBox(height: 25),
 
-          // cart tile
+              // shop tile
+              MyListTile(
+                text: 'Shop',
+                icon: Icons.home,
+                onTap: () => Navigator.pop(context),
+              ),
+
+              // cart tile
+              MyListTile(
+                text: 'Cart',
+                icon: Icons.shopping_cart,
+                onTap: () {},
+              ),
+            ],
+          ),
 
           // ezit shop title
+          Padding(
+            padding: const EdgeInsets.only(bottom: 25.0),
+            child: MyListTile(
+              text: 'Exit',
+              icon: Icons.logout,
+              onTap: () {},
+            ),
+          ),
         ],
       ),
     );
